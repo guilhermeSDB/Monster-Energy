8198cfa70c5866ceb3c51ac12c83fb9d71924b30
Guilherme Barros <Guilhermescarmagnani@outlook.com>
2022-01-05 22:55:47 -0300

    tradução das descrição

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ $('a').click(function(e){
 
 
 var monsterEnergy = [
-                    ['MEGA MONSTER ENERGY','Tear into a can of the meanest energy drink on the planet, Monster Energy.','src/images/monster1.png'],
+                    ['MEGA MONSTER ENERGY','Rasgue uma lata da bebida energética mais cruel do planeta, o Monster Energy.','src/images/monster1.png'],
                     ['Monster Energy Lo-Carb','Monster Energy Lo-Carb traz o mesmo sabor e o mesmo punch energético do original, com menos carboidratos e menos calorias.','src/images/monster1.2.png'],
                     ['Monster Energy Absolutely Zero','Monster Energy ZERO traz o mesmo sabor e o mesmo punch energético do original, com menos calorias e SEM açúcares.','src/images/monster1.3.png']
 ];
